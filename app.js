415190242986e0e2a4e80928d6367c3094482dc3
Nicole Uva <nicouva@gmail.com>
2020-11-03 21:15:38 -0500

    function update

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
 const quizContainer = document.getElementById('quiz')
 const resultsContainer = document.getElementById('results')
 const submit = document.getElementById('submit')
+const questionText = document.getElementById('question')
+const answers = document.getElementsByClassName('answers')
+const score = 0
+const timer = 30
+let currentQuestNumber = 0
 
 const quizQuestions = [
   {
     question: "Who invented JavaScript?",
-    answers: {
-      a: "Douglas Crockford",
-      b: "Mark Zuckerburg",
-      c: "Brendan Eich",
-      d: "Boutros-Boutros Ghali"
-    },
-    correctAnswer: "c"
+    choices: ["Douglas Crockford", "Mark Zuckerburg", "Brendan Eich", "Boutros-Boutros Ghali"],
+    correctAnswer: "Brendan Eich"
   },
   {
     question: "Which one of these is JavaScript?",
@@ -55,6 +55,21 @@ const quizQuestions = [
   }
 ]
 
-function buildQuiz() {
-
+function displayQuestion() {
+  let currentQuestion = quizQuestions[currentQuestNumber]
+  document.getElementById('question').textContent = currentQuestion.question
+  for (i = 0; i < answers.length; i++) {
+    answers[i].textContent = currentQuestion.choices[i]
+    answers[i].addEventListener('click', function (e) {
+      console.log(currentQuestion.choices[i])
+      console.log(currentQuestion.correctAnswer)
+      if (e.target.textContent === currentQuestion.correctAnswer) {
+        console.log('Correct Answer')
+      } else { console.log('Wrong Answer') }
+      currentQuestNumber += 1
+      displayQuestion
+    })
+  }
 }
+displayQuestion()
+
